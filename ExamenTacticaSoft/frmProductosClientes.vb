d8878f3c10e017530930419fb13b352ad7547790
agust <agustin198ar@gmail.com>
2025-06-30 15:55:50 -0300

    fix funciones botones

--- ExamenTacticaSoft/frmProductosClientes.vb
+++ ExamenTacticaSoft/frmProductosClientes.vb
@@ -1,15 +1,12 @@
 ﻿Public Class frmProductosClientes
-    Private currentBtn As Button = Nothing
+    Private currentBtn As Button = Nothing 'Para los botones activos
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblFiltroRapido.Click
-
-    End Sub
     Private Sub ActivarBoton(btn As Button)
         If btn IsNot Nothing Then
-            ' Si hay un botón activo anterior, lo desactivo
+            'Si hay un botón activo anterior, lo desactivo
             DesactivarBoton()
 
-            ' Estilo de botón activo
+            'Estilo de boton activo
             btn.ForeColor = Color.Crimson
             btn.Font = New Font(btn.Font.FontFamily, btn.Font.Size + 2, FontStyle.Bold)
 
@@ -19,7 +16,7 @@
 
     Private Sub DesactivarBoton()
         If currentBtn IsNot Nothing Then
-            ' Volver a color y tamaño original
+            'Volver a color y tamaño original
             currentBtn.ForeColor = Color.Black
             currentBtn.Font = New Font(currentBtn.Font.FontFamily, 9.75F, FontStyle.Regular)
             currentBtn = Nothing
