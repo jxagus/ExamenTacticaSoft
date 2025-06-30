904c10cceaf67ce4fe6198807e3b481e245ff3fe
agust <agustin198ar@gmail.com>
2025-06-30 13:40:19 -0300

    Clase Producto( Id,Nombre,Precio, Categoria)

--- Dominio/Productos.vb
+++ Dominio/Productos.vb
@@ -1,3 +1,42 @@
-﻿Public Class Productos
+﻿Public Class Producto 'Id, Nombre, Precioo, Categoria
+    Private _Id As Integer
+    Private _Nombre As String
+    Private _Precio As Decimal
+    Private _Categoria As String
 
+    Public Property Id As Integer
+        Get
+            Return _Id
+        End Get
+        Set(value As Integer)
+            _Id = value
+        End Set
+    End Property
+
+    Public Property Nombre As String
+        Get
+            Return _Nombre
+        End Get
+        Set(value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Public Property Precio As Decimal
+        Get
+            Return _Precio
+        End Get
+        Set(value As Decimal)
+            _Precio = value
+        End Set
+    End Property
+
+    Public Property Categoria As String
+        Get
+            Return _Categoria
+        End Get
+        Set(value As String)
+            _Categoria = value
+        End Set
+    End Property
 End Class
