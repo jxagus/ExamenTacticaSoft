28d3b4fe325587323e5fb797cd7c1016f6040e43
agust <agustin198ar@gmail.com>
2025-07-02 15:50:16 -0300

    ajuste dom-itemVenta

---
+++ Dominio/itemVenta.vb
@@ -0,0 +1,6 @@
+﻿Public Class ItemVenta
+    Public Property NombreProducto As String
+    Public Property PrecioUnitario As Decimal
+    Public Property Cantidad As Decimal
+    Public Property PrecioTotal As Decimal
+End Class
