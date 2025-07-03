e147ac47f024fa004166761ac1fe68ec71c3032f
agust <agustin198ar@gmail.com>
2025-07-03 20:34:07 -0300

    frmModificarVenta + cb

---
+++ ExamenTacticaSoft/frmModificarVenta.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmModificarVenta
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFecha = New System.Windows.Forms.Label()
+        Me.dtpFecha = New System.Windows.Forms.DateTimePicker()
+        Me.Total = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnGuardarCambios = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblFecha
+        '
+        Me.lblFecha.AutoSize = True
+        Me.lblFecha.Location = New System.Drawing.Point(51, 69)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(37, 13)
+        Me.lblFecha.TabIndex = 0
+        Me.lblFecha.Text = "Fecha"
+        '
+        'dtpFecha
+        '
+        Me.dtpFecha.Location = New System.Drawing.Point(54, 103)
+        Me.dtpFecha.Name = "dtpFecha"
+        Me.dtpFecha.Size = New System.Drawing.Size(198, 20)
+        Me.dtpFecha.TabIndex = 1
+        '
+        'Total
+        '
+        Me.Total.AutoSize = True
+        Me.Total.Location = New System.Drawing.Point(51, 152)
+        Me.Total.Name = "Total"
+        Me.Total.Size = New System.Drawing.Size(41, 13)
+        Me.Total.TabIndex = 2
+        Me.Total.Text = "lblTotal"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(51, 184)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 3
+        '
+        'btnGuardarCambios
+        '
+        Me.btnGuardarCambios.Location = New System.Drawing.Point(51, 229)
+        Me.btnGuardarCambios.Name = "btnGuardarCambios"
+        Me.btnGuardarCambios.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardarCambios.TabIndex = 4
+        Me.btnGuardarCambios.Text = "Guardar cambios"
+        Me.btnGuardarCambios.UseVisualStyleBackColor = True
+        '
+        'frmModificarVenta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(313, 360)
+        Me.Controls.Add(Me.btnGuardarCambios)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Total)
+        Me.Controls.Add(Me.dtpFecha)
+        Me.Controls.Add(Me.lblFecha)
+        Me.Name = "frmModificarVenta"
+        Me.Text = "frmModificarVenta"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblFecha As Label
+    Friend WithEvents dtpFecha As DateTimePicker
+    Friend WithEvents Total As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnGuardarCambios As Button
+End Class
---
+++ ExamenTacticaSoft/frmModificarVenta.vb
@@ -0,0 +1,38 @@
+﻿Imports Dominio
+Imports Negocio
+
+Public Class frmModificarVenta
+
+    Private idVenta As Integer
+    Private ventaOriginal As Venta
+
+    Public Sub New(venta As Venta)
+        InitializeComponent()
+        ventaOriginal = venta
+        idVenta = venta.Id
+    End Sub
+
+    Private Sub frmModificarVenta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Mostrar los datos actuales
+        dtpFecha.Value = ventaOriginal.Fecha
+        txtTotal.Text = ventaOriginal.Total.ToString("0.00")
+    End Sub
+
+    Private Sub btnGuardarCambios_Click(sender As Object, e As EventArgs) Handles btnGuardarCambios.Click
+        Dim nuevaFecha As DateTime = dtpFecha.Value
+        Dim nuevoTotal As Decimal
+
+        If Not Decimal.TryParse(txtTotal.Text, nuevoTotal) Then
+            MessageBox.Show("El total ingresado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim negocio As New VentaNegocio()
+        negocio.ModificarVenta(idVenta, nuevaFecha, nuevoTotal)
+
+        MessageBox.Show("Venta modificada con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Me.DialogResult = DialogResult.OK
+        Me.Close()
+    End Sub
+
+End Class
